6c15819f6af44909f02813d89075db738df2055c
shuvo-14 <66715106+shuvo-14@users.noreply.github.com>
2021-08-05 15:36:03 +0600

    Create 1.template (shuvo).cpp

---
+++ Range Query/Segment Tree/1.template (shuvo).cpp
@@ -0,0 +1,70 @@
+int n;
+struct segtree{
+    
+    int size;
+    vector<ll>tree;
+
+    void init(int n){
+        size = 1;
+        while(size < n) size *= 2;
+        tree.assign(2*size,0LL);
+        
+    }
+
+    //change accordingly
+    int merge(int u, int v)
+    {
+        return __gcd(u,v);
+    }
+
+    void build(int x, int lx, int rx,vector<int>&vec)
+    {
+        if(lx == rx)
+        {
+            tree[x] = vec[lx];
+            return;
+        }
+        int mid = (lx+rx)/2;
+        build(2*x+1,lx,mid,vec);
+        build(2*x+2,mid+1,rx,vec);
+        tree[x] = merge(tree[2*x+1],tree[2*x+2]);
+    }
+
+    void build(vector<int>&vec)
+    {
+        build(0,0,n-1,vec);
+    }
+
+    void update(int x, int lx, int rx, int i, int v)
+    {
+        if(lx == rx)
+        {
+            tree[x] = v;
+            return;
+        }
+        int mid = (lx+rx)/2;
+        if(i <= mid) update(2*x+1,lx,mid,i,v);
+        else update(2*x+2,mid+1,rx,i,v);
+        tree[x] = merge(tree[2*x+1], tree[2*x+2]);
+    }
+
+    void update(int i, int v){
+        update(0,0,n-1,i,v);     
+    }
+
+    ll query(int l, int r, int x, int lx, int rx)
+    {
+        if(lx > r || rx < l) return 0LL; //change accordingly
+        if(lx >= l && rx <= r) return tree[x];
+        int mid = (lx+rx)/2;
+        int s1 = query(l,r,2*x+1,lx,mid);
+        int s2 = query(l,r,2*x+2,mid+1,rx);
+        return merge(s1,s2);
+    }
+
+    ll query(int l, int r)
+    {
+        return query(l,r,0,0,n-1);
+    }
+
+};
