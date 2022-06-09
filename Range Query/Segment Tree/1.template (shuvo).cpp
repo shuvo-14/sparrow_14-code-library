7338d431a24c6ed76e8282e7182dabc54e418077
shuvo-14 <66715106+shuvo-14@users.noreply.github.com>
2022-06-09 08:09:50 +0600

    Update 1.template (shuvo).cpp

--- Range Query/Segment Tree/1.template (shuvo).cpp
+++ Range Query/Segment Tree/1.template (shuvo).cpp
@@ -1,80 +1,49 @@
-struct item{
-    //Use required attributes
-    int mn, mncnt, mx, mxcnt;
+int a[N];
 
-    //Default Values
-    item(int _mn = inf, int _mncnt = 0, int _mx = inf, int _mxcnt = 0)
+struct segtree 
+{
+    int sz = 1;
+    vector<int>tree;
+    void init(int n)
     {
-        mn = _mn, mx = _mx, mncnt = _mncnt, mxcnt = _mxcnt;
+        while(sz < n) sz *= 2;
+        tree.assign(2*sz+1,0);
     }
-};
-int n;
-struct segtree{
-    
-    int size;
-    vector<ll>tree;
-
-    void init(int n){
-        size = 1;
-        while(size < n) size *= 2;
-        tree.assign(2*size,0LL);
-        
-    }
-
-    //change accordingly
-    int merge(int u, int v)
+    int merge(int lft, int rgt)
     {
-        return __gcd(u,v);
+        return min(lft,rgt);
     }
-
-    void build(int x, int lx, int rx,vector<int>&vec)
+    void build(int x, int lx, int rx)
     {
         if(lx == rx)
         {
-            tree[x] = vec[lx];
+            tree[x] = a[lx];
             return;
         }
         int mid = (lx+rx)/2;
-        build(2*x+1,lx,mid,vec);
-        build(2*x+2,mid+1,rx,vec);
-        tree[x] = merge(tree[2*x+1],tree[2*x+2]);
+        build(2*x,lx,mid);
+        build(2*x+1,mid+1,rx);
+        tree[x] = merge(tree[2*x],tree[2*x+1]);
     }
-
-    void build(vector<int>&vec)
-    {
-        build(0,0,n-1,vec);
-    }
-
-    void update(int x, int lx, int rx, int i, int v)
+    void update(int x, int lx, int rx, int i, int val)
     {
         if(lx == rx)
         {
-            tree[x] = v;
+            tree[x] = val;
             return;
         }
         int mid = (lx+rx)/2;
-        if(i <= mid) update(2*x+1,lx,mid,i,v);
-        else update(2*x+2,mid+1,rx,i,v);
-        tree[x] = merge(tree[2*x+1], tree[2*x+2]);
+        if(i <= mid) update(2*x,lx,mid,i,val);
+        else update(2*x+1,mid+1,rx,i,val);
+        tree[x] = merge(tree[2*x],tree[2*x+1]);
     }
-
-    void update(int i, int v){
-        update(0,0,n-1,i,v);     
-    }
-
-    ll query(int l, int r, int x, int lx, int rx)
+    int query(int x, int lx, int rx, int l, int r)
     {
-        if(lx > r || rx < l) return 0LL; //change accordingly
-        if(lx >= l && rx <= r) return tree[x];
+        if(lx >= l and rx <= r) return tree[x];
+        if(lx > r or rx < l) return inf;
         int mid = (lx+rx)/2;
-        int s1 = query(l,r,2*x+1,lx,mid);
-        int s2 = query(l,r,2*x+2,mid+1,rx);
-        return merge(s1,s2);
+        int q1 = query(2*x,lx,mid,l,r);
+        int q2 = query(2*x+1,mid+1,rx,l,r);
+        return merge(q1,q2);
     }
-
-    ll query(int l, int r)
-    {
-        return query(l,r,0,0,n-1);
-    }
-
 };
