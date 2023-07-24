5555b6b79e7daa9987a0711ce9d72f7d8f1736c9
shuvo-14 <66715106+shuvo-14@users.noreply.github.com>
2023-07-24 16:37:41 +0600

    Update ordered set.cpp

--- ordered set.cpp
+++ ordered set.cpp
@@ -1,13 +1,20 @@
-#include <ext/pb_ds/assoc_container.hpp>
-#include <ext/pb_ds/tree_policy.hpp>
-using namespace __gnu_pbds ;
-typedef tree <pii, null_type, less< pii >, rb_tree_tag, tree_order_statistics_node_update > ordered_set;
-typedef tree <pii, null_type, less_equal< pii >, rb_tree_tag, tree_order_statistics_node_update > ordered_multiset;
+#include<bits/stdc++.h>
  
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+ 
+using namespace std;
+using namespace __gnu_pbds;
+ 
+template<class T>
+using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update> ;
+template<typename T> 
+using ordered_multiset = tree<T, null_type,less_equal<T>, rb_tree_tag,tree_order_statistics_node_update>;
 /*
 Note :  There is a problem with erase() function in ordered_multiset (for less_equal<int> tag).
         lower_bound() works as upper_bound() and vice-versa.
         Be careful to use.
-        i) find_by_order(k) : kth smallest element counting from 0 .
+        i) find_by_order(k) : returns an iterator to the kth smallest element counting from 0 .
         ii) order_of_key(k) : number of elements strictly smaller than k.
+        iii) to erase x :  st.erase(st.find_by_order(st.order_of_key(x)). 
 */
