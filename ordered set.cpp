2d7aecf51baafc934c51e96be6eab410862d53bd
shuvo-14 <66715106+shuvo-14@users.noreply.github.com>
2023-07-24 11:04:46 +0600

    Create ordered set.cpp

---
+++ ordered set.cpp
@@ -0,0 +1,13 @@
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+
+typedef tree <pii, null_type, less< pii >, rb_tree_tag, tree_order_statistics_node_update > ordered_set;
+typedef tree <pii, null_type, less_equal< pii >, rb_tree_tag, tree_order_statistics_node_update > ordered_multiset;
+ 
+/*
+Note :  There is a problem with erase() function in ordered_multiset (for less_equal<int> tag).
+        lower_bound() works as upper_bound() and vice-versa.
+        Be careful to use.
+        i) find_by_order(k) : kth smallest element counting from 0 .
+        ii) order_of_key(k) : number of elements strictly smaller than k.
+*/
